8d3feb82093e21a9e0f0683ec5feecb88ed90ea8
Jhonatan Quishpe <jhonatanqt97@gmail.com>
2023-11-21 19:06:34 +0000

    snapshots created and stg_orders tranform to incremental model

--- models/staging/sql_server_dbo/stg_orders.sql
+++ models/staging/sql_server_dbo/stg_orders.sql
@@ -1,10 +1,20 @@
-with 
+{{ config(
+    materialized='incremental',
+    unique_key= 'order_id',
+    on_schema_change='fail'
+    ) 
+    }}
 
-source as (
 
-    select * from {{ source('sql_server_dbo', 'orders') }}
+WITH stg_orders AS (
+    SELECT * 
+    FROM {{ source('sql_server_dbo','orders') }}
+{% if is_incremental() %}
 
-),
+	  where _fivetran_synced > (select max(_fivetran_synced) from {{ this }})
+
+{% endif %}
+    ),
 
 renamed as (
 
@@ -13,8 +23,8 @@ renamed as (
         decode(shipping_service,'','vacio',shipping_service) AS shipping_service,
         shipping_cost,
         address_id,
-        cast(created_at as timestamp_ltz) as created_at  ,
-        decode(promo_id,'','sin promo',{{ dbt_utils.generate_surrogate_key(['promo_id'])}}) AS promo_id,
+        created_at,
+        decode(promo_id,'','9999', promo_id) AS promo_id,
         decode(estimated_delivery_at,null,'9999',estimated_delivery_at) AS estimated_delivery_at,
         order_cost,
         user_id,
@@ -25,11 +35,38 @@ renamed as (
         _fivetran_deleted,
         _fivetran_synced
 
-    from source
-)
-
+    from stg_orders
 
  
+),
 
+renamed_casted as (
 
-select * from renamed
+    select
+        order_id,
+        shipping_service,
+        shipping_cost,
+        address_id,
+        cast(created_at as timestamp_ltz) as created_at,
+        cast(
+            case
+             when promo_id= '9999' then '9999'
+             else {{ dbt_utils.generate_surrogate_key(['promo_id'])}}
+             end as varchar(50) 
+        ) as promo_id,
+        cast(estimated_delivery_at as timestamp_ltz) as estimated_delivery_at,
+        order_cost,
+        user_id,
+        order_total,
+        cast(delivered_at as timestamp_ltz) as delivered_at,
+        tracking_id,
+        status,
+        _fivetran_deleted,
+        _fivetran_synced
+
+    from renamed
+
+)
+   
+
+select * from renamed_casted
